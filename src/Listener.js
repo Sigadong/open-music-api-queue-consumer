e57e2efbcc22f9ac049499eceea4581d66185f2e
Sigadong <irwanto.regar30@gmail.com>
2021-07-08 00:39:45 +0700

    consumer program finished | open music apps

---
+++ src/Listener.js
@@ -0,0 +1,21 @@
+class Listener {
+  constructor(PlaylistsService, mailSender) {
+    this._PlaylistsService = PlaylistsService;
+    this._mailSender = mailSender;
+    this.listen = this.listen.bind(this);
+  }
+
+  async listen(message) {
+    try {
+      const { userId, targetEmail } = JSON.parse(message.content.toString());
+
+      const playlists = await this._PlaylistsService.getPlaylists(userId);
+      const resultPlaylists = await this._mailSender.sendEmail(targetEmail, JSON.stringify(playlists));
+      console.log(resultPlaylists);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+module.exports = Listener;
---
+++ src/MailSender.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+
+class MailSender {
+  constructor() {
+    this._transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.MAIL_ADDRESS,
+        pass: process.env.MAIL_PASSWORD,
+      },
+    });
+  }
+
+  sendEmail(targetEmail, content) {
+    const messagePlaylists = {
+      from: 'Open Music Apps',
+      to: targetEmail,
+      subject: 'Export Playlists',
+      text: 'Terlampir hasil dari ekspor playlists',
+      attachments: [
+        {
+          filename: 'playlists.json',
+          content,
+        },
+      ],
+    };
+
+    return this._transporter.sendMail(messagePlaylists);
+  }
+}
+
+module.exports = MailSender;
